207e0c09cf6dadba7776d05af6b6f810d4867602
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-06-07 23:08:42 -0300

    Get movie details info to the page

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,15 +1,35 @@
-import React, {useContext} from 'react';
-import { IdMovieContext}  from './../contexts/IdMovieContext';
+import React, {useContext, useEffect, useState} from 'react';
+import { IdMovieContext }  from './../contexts/IdMovieContext';
 
 const MovieDetails = () => {
 
   const { idMovie } = useContext(IdMovieContext);
-  
-    return (
-      
-    <h1> { idMovie } </h1>
 
-      );
+ const [movieDetails, setMovieDatails] = useState("");
+
+  async function getMovieDetails(id) { 
+    let response = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=ab72c6b9`);
+    let data = await response.json();
+    console.log(data);
+    if(data.Response !== "False") setMovieDatails(data);    
+  };
+
+  useEffect( () => {  
+    if(!movieDetails) getMovieDetails(idMovie)
+  });
+
+  const toRender = () => {
+    if(!movieDetails) return <h1>Movie not found!</h1>
+    return(
+      <h1>{movieDetails.Title}</h1>
+    )
+  }
+
+  return (
+    <div>
+      { toRender() }
+    </div>
+  );
 }
  
 export default MovieDetails;
