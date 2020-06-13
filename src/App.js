b929d5778dbd5673e794d6990696ec27e60bcc14
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-06-13 08:34:52 -0300

    App.js refactored

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom"
-import SearchMovieForm from './components/SearchMovieForm';
-import IdMovieProvider from './contexts/IdMovieContext';
-import MovieDetails from './components/MovieDetails';
-import Header from './components/Header/Header';
-import About from './components/About';
-import Footer from './components/Header/Footer';
 import LightDarkModeProvider from './contexts/LightDarkModeContext';
-
-import { useContext } from 'react';
-import { LightDarkModeContext } from './contexts/LightDarkModeContext';
-
-const BodyApp = () => {
-  const { isDarkMode } = useContext(LightDarkModeContext);
-  
-  return ( 
-    <div className={`${isDarkMode? 'bg-secondary h-100': ''}`}>
-        <BrowserRouter>          
-            <Header />
-            <IdMovieProvider>                 
-                                          
-              <Route path="/" exact component={SearchMovieForm} />
-              <Route path="/details" component={MovieDetails} />
-              <Route path="/about" component={About} />                
-                  
-            </IdMovieProvider>
-            <Footer />            
-        </BrowserRouter>
-    </div>
-   );
-}
+import BodyApp from './components/BodyApp';
 
 function App() {
   document.title = "Movie Finder"
---
+++ src/components/BodyApp.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useContext } from 'react';
+import {BrowserRouter, Route} from "react-router-dom"
+
+import IdMovieProvider from './../contexts/IdMovieContext'
+import { LightDarkModeContext } from './../contexts/LightDarkModeContext';
+
+import SearchMovieForm from './SearchMovieForm'
+import MovieDetails from './MovieDetails';
+import Header from './Header/Header';
+import About from './About';
+import Footer from './Header/Footer';
+
+const BodyApp = () => {
+    const { isDarkMode } = useContext(LightDarkModeContext);
+    
+    return ( 
+      <div className={`${isDarkMode? 'bg-secondary h-100': ''}`}>
+          <BrowserRouter>          
+              <Header />
+              <IdMovieProvider>                 
+                                            
+                <Route path="/" exact component={SearchMovieForm} />
+                <Route path="/details" component={MovieDetails} />
+                <Route path="/about" component={About} />                
+                    
+              </IdMovieProvider>
+              <Footer />            
+          </BrowserRouter>
+      </div>
+     );
+  }
+
+  export default BodyApp;
