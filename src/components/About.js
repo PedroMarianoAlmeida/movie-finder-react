a12e8bbeddec172c2c04d31f57adc132ea06154f
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2021-12-16 21:57:28 -0300

    formatação do codigo

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div className='mt-5 pt-3 container'>
-        <div className='row justify-content-center'>
+  return (
+    <div className="mt-5 pt-3 container">
+      <div className="row justify-content-center">
+        <div className="col-12 text-center">
+          <h1>About</h1>
+        </div>
 
-          <div className="col-12 text-center">
-            <h1>About</h1>
-          </div>
-
-          <div className="col-12 mt-3">
-            <p>This is a WebApp made in React, it consumes the <a href="https://www.omdbapi.com/" target="_blank" rel="noopener noreferrer">OMDb API</a> to search movies/series with the name that user input in the search box.</p>
-            <p>This tool is part of the Pedro Almeida portfolio. Please check my website in the footer section to contact me and/or see my other projects.</p>
-          </div>
-
+        <div className="col-12 mt-3">
+          <p>
+            This is a WebApp made in React, it consumes the{" "}
+            <a
+              href="https://www.omdbapi.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              OMDb API
+            </a>{" "}
+            to search movies/series with the name that user input in the search
+            box.
+          </p>
+          <p>
+            This tool is part of the Pedro Almeida portfolio. Please check my
+            website in the footer section to contact me and/or see my other
+            projects.
+          </p>
         </div>
       </div>
-    );
-}
- 
+    </div>
+  );
+};
+
 export default About;
